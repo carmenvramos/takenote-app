fac436d806ae4b8074d0adf79a907f0a185e16a7
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-25 15:25:22 -0700

    Added Header & CSS

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Header } from '../header/Header';
+
 export default function App() {
-  return <h1>Welcome to TakeNote App</h1>;
+  return <Header/>;
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Header.css';
+
+export function Header() {
+  return (
+    <header className={styles.Header}>
+      <h1>TAKE NOTE</h1>
+    </header>
+  );
+}
